cb309fddb678216abc67284e79b0406451204456
Melanie Budde <melanie.budde@bergwerk.io>
2018-01-15 03:11:11 +0100

    extract game data for pair

--- extract.py
+++ extract.py
@@ -2,15 +2,15 @@
 import scipy.io
 import numpy as np
 import properties as pr
-import matplotlib.pyplot as mp
+import matplotlib.pyplot as plt
 
 # Raw data
-data = scipy.io.loadmat('Data_BallGame_6pairs.mat.mat')
+data = scipy.io.loadmat('Data/Data_BallGame_6pairs.mat.mat')
 data = data["data_over_trials"]
 print(data.shape) # (6,3) => 6 games with three columns, that are first player, second player, some information
 #print(data)
 # contains meta-information for all pairs in data
-meta = scipy.io.loadmat('meta_info_6pairs.mat')
+meta = scipy.io.loadmat('Data/meta_info_6pairs.mat')
 meta = meta["meta_info"]
 print(meta.shape) # (6,2) => info for 6 games with two columns, that contain information about first player and second player
 
@@ -21,10 +21,15 @@ print("Info player 2: %s" % pair0[1])
 
 # extract data from game 0
 game = 0
-(p1, p2, pair) = data[game] #(32, 961, 140) (info, frame, trial)
-print(p1.shape) #
-print(p2.shape) # (21, 961, 140)
-print(pair.shape) # (1,1)
+(p1, p2, pair_meta) = data[game] #(info, frame, trial)
+#print(p1.shape) #(32, 961, 140)
+#print(p2.shape) # (21, 961, 140)
+#print(pair.shape) # (1,1)
+
+
+pair = p1[pr.START_PAIR : pr.END_PAIR]
+print("Proximitry to obstacle", pair)
+print(pair_meta)
 
 #####################################################################STOP ab hier macht es keinen Sinn mehr, der code kann weg
 # check who sees the obstacle in trial
@@ -45,13 +50,19 @@ trial = 139
 
 #print(p1[])
 
+#extract x and y coordinates of ball and if a obstacle was hit
+hits = []
 
 for frameNr in range(0, len(p1[1])):
-  x = p1[pr.X_POS][frameNr][trial]
-  y = p1[pr.Y_POS][frameNr][trial]
+  x = p1[pr.X_POS_BALL][frameNr][trial]
+  y = p1[pr.Y_POS_BALL][frameNr][trial]
   hit = p1[pr.OBSTACLE_HIT][frameNr][trial]
+  hits.append(hit)
   print("x und y coordinate: %d %d and obstacle hit:%d" % (x,y, hit))
 
-# x and y coordinates
-mainline = plt.plot(results, 'r-', alpha=0.8)
-plt.setp(mainline, color='red', linewidth=2.0)
+# hits
+mainline = plt.plot(hits, 'r-', alpha=0.8) #der eigentlich plot
+plt.setp(mainline, color='red', linewidth=2.0)# einstellung zeichnung
+
+plt.ylabel('hits')
+plt.show()
